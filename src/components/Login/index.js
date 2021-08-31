c74a8c07ed58e36601d11f76dc82cefcfda94f88
gstadtler <ghstadtler@gmail.com>
2021-08-31 14:14:56 -0300

    Estilizando o component de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import { AuthContext } from "../providers/auth";
 import { authenticate } from "../../services/requestFunctions";
 
-import "./styles.css";
+import "./styles.scss";
 
 const Login = () => {
   const auth = useContext(AuthContext);
@@ -38,44 +38,52 @@ const Login = () => {
         </div>
       )}
       {!auth.isAuthenticated && (
-        <div>
-          <h4>Informe suas credenciais do strateegia.digital</h4>
-          <form className="login" onSubmit={handleSubmit(handleLogin)}>
-            <FormControl isInvalid={errors.email}>
-              <Input
-                id="email"
-                type="email"
-                placeholder="Email"
-                {...register("email", {
-                  required: "This is required",
-                })}
-              />
-              <FormErrorMessage>
-                {errors.email && errors.email.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.password}>
-              <Input
-                id="password"
-                type="password"
-                placeholder="Password"
-                {...register("password", {
-                  required: "This is required",
-                })}
-              />
-              <FormErrorMessage>
-                {errors.password && errors.password.message}
-              </FormErrorMessage>
-            </FormControl>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </form>
+        <div className="login-container">
+          <div className="informative">
+            <p>
+              Debata assuntos do momento com privacidade e de forma estruturada
+            </p>
+          </div>
+          <div className="login-wrapper">
+            <h2>login</h2>
+            <p>Insira seus dados de login do Strateegia</p>
+            <form className="login-form" onSubmit={handleSubmit(handleLogin)}>
+              <FormControl isInvalid={errors.email}>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: "This is required",
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.password}>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  {...register("password", {
+                    required: "This is required",
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                type="submit"
+              >
+                entrar
+              </Button>
+            </form>
+          </div>
         </div>
       )}
     </>
