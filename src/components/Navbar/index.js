a13e9b8ccaba299c50f5d99052997b2902613095
gstadtler <ghstadtler@gmail.com>
2021-08-31 13:17:40 -0300

    Component de navegação

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { useHistory } from "react-router";
+import { AuthContext } from "../providers/auth";
+import "./styles.css";
+
+const Navbar = () => {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    auth.setApiToken("");
+    auth.setIsAuthenticated(!auth.isAuthenticated);
+    history.push("/login");
+  };
+
+  return (
+    <>
+      <div className="navbar">
+        <h1>Trending Strateegia</h1>
+        <div className="logout">
+          <h3>Bem vinda!</h3>
+          <button onClick={handleLogout}>logout</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
