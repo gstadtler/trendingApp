de7849017845bc2d005ad70b70bd2d9173c3d639
gstadtler <ghstadtler@gmail.com>
2021-09-16 03:11:51 -0300

    Usa dados de mentira enquanto não carrega trends

--- src/components/TrendsList/index.js
+++ src/components/TrendsList/index.js
@@ -3,6 +3,7 @@ import { HiSearch } from "react-icons/hi";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { Input, Box } from "@chakra-ui/react";
 import { fetchTrendingTopics } from "../../services/trending_server";
+import mockTrends from "../../utils/mock_data.json";
 import {
   Pagination,
   PaginationContainer,
@@ -19,7 +20,7 @@ import TrendsListItem from "../TrendsListItem";
 
 const TrendsList = ({ handleKitData }) => {
   const [trends, setTrends] = useState([]);
-  const [trendsLocation, setTrendsLocation] = useState({});
+  // const [trendsLocation, setTrendsLocation] = useState({});
   const [searchInput, setSearchInput] = useState("");
 
   const trendsOuterLimit = 2;
@@ -52,7 +53,7 @@ const TrendsList = ({ handleKitData }) => {
   useEffect(() => {
     fetchTrendingTopics().then((data) => {
       setTrends(data[0].trends);
-      setTrendsLocation(data[0].locations);
+      // setTrendsLocation(data[0].locations);
     });
   }, []);
 
@@ -166,7 +167,107 @@ const TrendsList = ({ handleKitData }) => {
         </div>
       ) : (
         <div>
-          <h1>loading...</h1>
+          <div className="trending-topics-container">
+            <header>
+              <HiSearch size={20} />
+              <Input
+                placeholder="procurar assunto"
+                onChange={(e) => setSearchInput(e.target.value)}
+              />
+            </header>
+            <Box className="trends-wrapper">
+              <TrendsListItem
+                items={mockTrends}
+                curPage={currentPage - 1}
+                trendsLimit={pageSize}
+                handleKitData={handleKitData}
+                searchQuery={searchInput}
+              />
+            </Box>
+            <div className="pagination-ctrl">
+              <Pagination
+                pagesCount={pagesCount}
+                currentPage={currentPage}
+                isDisabled={isDisabled}
+                onPageChange={handlePageChange}
+              >
+                <PaginationContainer
+                  align="center"
+                  justify="space-between"
+                  p={4}
+                  w="full"
+                >
+                  <PaginationPrevious
+                    _hover={{
+                      opacity: "75%",
+                    }}
+                    border="1px solid #000"
+                    onClick={() =>
+                      console.log(
+                        "Im executing my own function along with Previous component functionality"
+                      )
+                    }
+                  >
+                    <GrFormPrevious size={20} />
+                  </PaginationPrevious>
+                  <PaginationPageGroup
+                    isInline
+                    align="center"
+                    separator={
+                      <PaginationSeparator
+                        onClick={() =>
+                          console.log(
+                            "Im executing my own function along with Separator component functionality"
+                          )
+                        }
+                        border="1px solid"
+                        fontSize="sm"
+                        w={7}
+                        jumpSize={5}
+                      />
+                    }
+                  >
+                    {pages.map((page) => (
+                      <PaginationPage
+                        w={7}
+                        border="none"
+                        key={`pagination_page_${page}`}
+                        page={page}
+                        onClick={() =>
+                          console.log(
+                            "Im executing my own function along with Page component functionality"
+                          )
+                        }
+                        fontSize="sm"
+                        _hover={{
+                          bg: "#dc0362",
+                        }}
+                        _current={{
+                          bg: "#dc0362",
+                          color: "#fff",
+                          fontSize: "sm",
+                          w: 7,
+                        }}
+                      />
+                    ))}
+                  </PaginationPageGroup>
+                  <PaginationNext
+                    _hover={{
+                      opacity: "75%",
+                    }}
+                    border="none"
+                    onClick={() =>
+                      console.log(
+                        "Im executing my own function along with Next component functionality"
+                      )
+                    }
+                  >
+                    <GrFormNext size={20} />
+                  </PaginationNext>
+                </PaginationContainer>
+              </Pagination>
+            </div>
+          </div>
         </div>
       )}
     </>
