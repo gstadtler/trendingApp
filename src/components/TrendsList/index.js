19f329ea2dca775769391c7c88ee37355819b964
gstadtler <ghstadtler@gmail.com>
2021-09-23 20:10:21 -0300

    Adiciona animação de carregamento dos trends

--- src/components/TrendsList/index.js
+++ src/components/TrendsList/index.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { HiSearch } from "react-icons/hi";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-import { Input, Box } from "@chakra-ui/react";
+import { Input, Box, CircularProgress } from "@chakra-ui/react";
 import { fetchTrendingTopics } from "../../services/trending_server";
-import mockTrends from "../../utils/mock_data.json";
 import {
   Pagination,
   PaginationContainer,
@@ -165,109 +164,11 @@ const TrendsList = ({ handleKitData }) => {
           </div>
         </div>
       ) : (
-        <div>
-          <div className="trending-topics-container">
-            <header>
-              <HiSearch size={20} />
-              <Input
-                placeholder="procurar assunto"
-                onChange={(e) => setSearchInput(e.target.value)}
-              />
-            </header>
-            <Box className="trends-wrapper">
-              <TrendsListItem
-                items={mockTrends}
-                curPage={currentPage - 1}
-                trendsLimit={pageSize}
-                handleKitData={handleKitData}
-                searchQuery={searchInput}
-              />
-            </Box>
-            <div className="pagination-ctrl">
-              <Pagination
-                pagesCount={pagesCount}
-                currentPage={currentPage}
-                isDisabled={isDisabled}
-                onPageChange={handlePageChange}
-              >
-                <PaginationContainer
-                  align="center"
-                  justify="space-between"
-                  p={4}
-                  w="full"
-                >
-                  <PaginationPrevious
-                    _hover={{
-                      opacity: "75%",
-                    }}
-                    border="1px solid #000"
-                    onClick={() =>
-                      console.log(
-                        "Im executing my own function along with Previous component functionality"
-                      )
-                    }
-                  >
-                    <GrFormPrevious size={20} />
-                  </PaginationPrevious>
-                  <PaginationPageGroup
-                    isInline
-                    align="center"
-                    separator={
-                      <PaginationSeparator
-                        onClick={() =>
-                          console.log(
-                            "Im executing my own function along with Separator component functionality"
-                          )
-                        }
-                        border="1px solid"
-                        fontSize="sm"
-                        w={7}
-                        jumpSize={5}
-                      />
-                    }
-                  >
-                    {pages.map((page) => (
-                      <PaginationPage
-                        w={7}
-                        border="none"
-                        key={`pagination_page_${page}`}
-                        page={page}
-                        onClick={() =>
-                          console.log(
-                            "Im executing my own function along with Page component functionality"
-                          )
-                        }
-                        fontSize="sm"
-                        _hover={{
-                          bg: "#dc0362",
-                        }}
-                        _current={{
-                          bg: "#dc0362",
-                          color: "#fff",
-                          fontSize: "sm",
-                          w: 7,
-                        }}
-                      />
-                    ))}
-                  </PaginationPageGroup>
-                  <PaginationNext
-                    _hover={{
-                      opacity: "75%",
-                    }}
-                    border="none"
-                    onClick={() =>
-                      console.log(
-                        "Im executing my own function along with Next component functionality"
-                      )
-                    }
-                  >
-                    <GrFormNext size={20} />
-                  </PaginationNext>
-                </PaginationContainer>
-              </Pagination>
-            </div>
-          </div>
-        </div>
+        <CircularProgress
+          color="#563a8e"
+          isIndeterminate
+          className="loading-trends"
+        />
       )}
     </>
   );
